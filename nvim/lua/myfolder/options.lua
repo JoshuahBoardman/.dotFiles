2d3503daffd167d1d19643d82a77f06eb2efc188
Joshuah Boardman <joshuahboardman@gmail.com>
2023-07-18 07:04:20 -0500

    Change: removed wrapping text in neovim

--- nvim/lua/myfolder/options.lua
+++ nvim/lua/myfolder/options.lua
@@ -13,7 +13,7 @@ local options = {
     termguicolors = true,
 
     smartindent = true,
-    wrap = true,
+    wrap = false,
 
     -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
     -- delays and poor user experience.
