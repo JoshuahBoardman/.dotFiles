b0b2dc06fa1705c5c82ef22608427872524ae6df
Joshuah Boardman <joshuahboardman@Joshuahs-Laptop.local>
2024-01-01 11:47:19 -0600

    Update: neovim config

---
+++ nvim/lua/joshuahBoardman/packer.lua
@@ -0,0 +1,111 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use) 
+    ---------------------
+    -- Package Manager --
+    ---------------------
+
+    -- Packer can manage itself
+    use "wbthomason/packer.nvim" -- May swap to lazy.nvim 
+
+    --------------------------
+    -- Package Dependencies --
+    --------------------------
+
+    use "nvim-lua/plenary.nvim"
+
+   ---------------------
+    -- Themes & Styles --
+    ---------------------
+    
+    use 'folke/tokyonight.nvim'
+    use { "catppuccin/nvim", as = "catppuccin" }
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+
+    ------------------------------
+    -- Better Syntax Highlights --
+    ------------------------------
+    
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+
+    use {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        }
+    }
+
+    --------------------------------
+    -- Navigation & Fuzzy Finding --
+    --------------------------------
+    
+    -- Install ripgrep & fn for improved experience 
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+
+    use {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        requires = { {"nvim-lua/plenary.nvim"} }
+    }
+  
+    use 'mbbill/undotree'
+
+    ----------------------------------
+    -- LSP, Completions, & Snippets --
+    ----------------------------------
+
+    use "hrsh7th/nvim-cmp" -- The completion plugin
+    use "hrsh7th/cmp-buffer" -- buffer completions
+    use "hrsh7th/cmp-path" -- path completions
+    use "hrsh7th/cmp-cmdline" -- cmdline completions
+    use "saadparwaiz1/cmp_luasnip" -- snippet completions
+    use "hrsh7th/cmp-nvim-lsp"
+    use "hrsh7th/cmp-nvim-lua"
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        }
+    }
+
+    --------------------------
+    -- Vim Practice & Games --
+    --------------------------
+    
+    use 'ThePrimeagen/vim-be-good'
+    
+end)
