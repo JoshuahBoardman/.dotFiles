35f1503adf8bc613a3a16b3d69a7615ec76be73b
Joshuah Boardman <joshuahboardman@gmail.com>
2024-06-02 21:02:10 -0500

    Fix: remove test config from alacritty.toml

--- nvim/lua/joshuahBoardman/plugin/obsidian.lua
+++ nvim/lua/joshuahBoardman/plugin/obsidian.lua
@@ -17,13 +17,21 @@ return {
                         -- obsidian vault path
                         path = "~/my_obsidian_vault/",
                     },
-                },
+                };
+
                 templates = {
                     -- obsidian template file path
                     folder = "~/my_obsidian_vault/meta_data/templates/",
                     date_format = "%Y-%m-%d-%a",
                     time_format = "%H:%M",
-                },
+                };
+
+                follow_url_func = function(url)
+                    -- Open the URL in the default web browser.
+                    vim.fn.jobstart({"open", url})  -- Mac OS
+                    -- vim.fn.jobstart({"xdg-open", url})  -- linux
+                end
+
             })
             vim.opt.conceallevel = 1
             -- TODO: add hotkeys for concistently used actions
