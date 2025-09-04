6308b295e32ca70fa208754f398ca142f4640950
JoshuahBoardman <joshuahboardman@gmail.com>
2025-09-04 04:43:27 +0000

    Add package list to config_data

--- scripts/bootstrap_env/config_data.sh
+++ scripts/bootstrap_env/config_data.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#########################################################
+#'a This script is used for system/enviorment config data #
+#########################################################
 
 ##########################
 #### Config Structure ####
@@ -31,7 +34,7 @@ managers["yay.install_flags"]=""
 ##################
 
 #TODO: Add property to store package config location
-package_keys=("git" "stow" "go" "fd" "gcc" "make" "ripgrep" "nodejs" "unzip" "neovim" "test")
+package_keys=("git" "stow" "go" "fd" "gcc" "make" "ripgrep" "nodejs" "unzip" "neovim" "test" "networkmanager" "intel-ucode" "mesa" "vulkan-intel" "lib32-mesa" "base-devel" "git" "yay" "wayland" "hyprland" "uwsm" "ghostty" "firefox" "pipewire" "wireplumber" "ttf-dejavu" "waybar" "hyprpaper" "neovim" "hyprshot" "swaync" "hyprlock" "zellij" "stow" "starship" "ly" "fastfetch" "obs-studio" "gimp")
 
 declare -A packages
 
@@ -65,6 +68,104 @@ packages["unzip.manager"]="pacman"
 packages["neovim.value"]="neovim"
 packages["neovim.manager"]="pacman"
 
-packages["test.value"]="test"
-packages["test.manager"]="testy"
+packages["networkmanager.value"]="networkmanager"
+packages["networkmanager.manager"]="pacman"
 
+packages["intel-ucode.value"]="intel-ucode" 
+packages["intel-ucode.manager"]="pacman"
+
+packages["mesa.value"]="mesa" 
+packages["mesa.manager"]="pacman"
+
+packages["vulkan-intel.value"]="vulkan-intel" 
+packages["vulkan-intel.manager"]="pacman"
+
+packages["lib32-mesa.value"]="lib32-mesa" 
+packages["lib32-mesa.manager"]="pacman"
+
+packages["lib32-vulkan-intel.value"]="lib32-vulkan-intel" 
+packages["lib32-vulkan-intel.manager"]="pacman"
+
+packages["base-devel.value"]="base-devel" 
+packages["base-devel.manager"]="pacman"
+
+packages["git.value"]="git" 
+packages["git.manager"]="pacman"
+
+packages["yay.value"]="yay" 
+packages["yay.manager"]="pacman"
+
+packages["wayland.value"]="wayland" 
+packages["wayland.manager"]="pacman"
+
+packages["hyprland.value"]="hyprland" 
+packages["hyprland.manager"]="pacman"
+
+packages["uwsm.value"]="uwsm" 
+packages["uwsm.manager"]="pacman"
+
+packages["ghostty.value"]="ghostty" 
+packages["ghostty.manager"]="pacman"
+
+packages["firefox.value"]="firefox" 
+packages["firefox.manager"]="pacman"
+
+packages["pipewire.value"]="pipewire" 
+packages["pipewire.manager"]="pacman"
+
+packages["wireplumber.value"]="wireplumber" 
+packages["wireplumber.manager"]="pacman"
+
+packages["ttf-dejavu.value"]="ttf-dejavu" 
+packages["ttf-dejavu.manager"]="pacman"
+
+packages["wofi.value"]="wofi" 
+packages["wofi.manager"]="pacman"
+
+packages["waybar.value"]="waybar" 
+packages["waybar.manager"]="pacman"
+
+packages["hyprpaper.value"]="hyprpaper" 
+packages["hyprpaper.manager"]="pacman"
+
+packages["neovim.value"]="neovim" 
+packages["neovim.manager"]="pacman"
+
+packages["hyprshot.value"]="hyprshot" 
+packages["hyprshot.wanager"]="pacman"
+
+packages["swaync.value"]="swaync" 
+packages["swaync.wanager"]="pacman"
+
+packages["hyprlock.value"]="hyprlock" 
+packages["hyprlock.wanager"]="pacman"
+
+packages["zellij.value"]="zellij" 
+packages["zellij.wanager"]="pacman"
+
+packages["stow.value"]="stow" 
+packages["stow.wanager"]="pacman"
+
+packages["starship.value"]="starship" 
+packages["starship.wanager"]="pacman"
+
+packages["ly.value"]="ly" 
+packages["ly.wanager"]="pacman"
+
+packages["fastfetch.value"]="fastfetch" 
+packages["fastfetch.wanager"]="pacman"
+
+packages["obs-studio.value"]="obs-studio" 
+packages["obs-studio.wanager"]="pacman"
+
+packages["gimp.value"]="gimp" 
+packages["gimp.wanager"]="pacman"
+
+packages["gimp.value"]="gimp" 
+packages["gimp.wanager"]="pacman"
+
+
+
+
+packages["test.value"]="test" #TODO:Remove this set
+packages["test.manager"]="testy"
