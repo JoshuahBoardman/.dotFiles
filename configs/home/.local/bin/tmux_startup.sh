0b77b2ba071d6b5a6a4997619f2e64fc4f8fd891
Joshuah Boardman <joshuahboardman@gmail.com>
2025-09-09 15:20:53 -0500

    Add neovim vault support tweeks

--- configs/home/.local/bin/tmux_startup.sh
+++ configs/home/.local/bin/tmux_startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Your default sessions
-default_sessions=("dev" "test" "server" "output")
+default_sessions=("dev" "test" "server" "output" "notes")
 
 # Kill all sessions not in the default list
 for s in $(tmux list-sessions -F "#{session_name}"); do
@@ -14,6 +14,8 @@ done
 for s in "${default_sessions[@]}"; do
     tmux has-session -t "$s" 2>/dev/null || tmux new-session -d -s "$s"
 done
+
+#TODO: Open notes straight to my vault
 
 # Attach to the first default session
 tmux attach-session -t "dev"
