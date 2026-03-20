658e66e18f1b343bfc40725c02ee126c22038513
Joshuah Boardman <joshuahboardman@gmail.com>
2026-03-20 15:47:56 -0500

    Update claude statusline config

--- configs/home/.claude/statusline-command.sh
+++ configs/home/.claude/statusline-command.sh
@@ -6,4 +6,10 @@ DIR=$(echo "$input" | jq -r '.workspace.current_dir')
 # The "// 0" provides a fallback if the field is null
 PCT=$(echo "$input" | jq -r '.context_window.used_percentage // 0' | cut -d. -f1)
 
-echo "[$MODEL] 📁 ${DIR##*/} | ${PCT}% context"
+IN=$(echo "$input" | jq -r '.context_window.total_input_tokens // 0')
+OUT=$(echo "$input" | jq -r '.context_window.total_output_tokens // 0')
+
+RATE_USE=$(echo "$input" | jq -r '.rate_limits.five_hour.used_percentage // 0' | cut -d. -f1)
+
+
+echo "[$MODEL] 📁 ${DIR##*/} | ${PCT}% context | ${IN} input | ${OUT} output | ${RATE_USE}% used"
