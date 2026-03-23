dabe727ce6700647a454144c0322f3d3cb34ad32
JoshuahBoardman <joshuahboardman@gmail.com>
2026-03-22 21:12:31 -0500

    Update Claude config files

--- configs/claude/statusline-command.sh
+++ configs/claude/statusline-command.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 input=$(cat)
 
-MODEL=$(echo "$input" | jq -r '.model.display_name')
-DIR=$(echo "$input" | jq -r '.workspace.current_dir')
+MODEL=$(echo "$input" | yq -r -p json '.model.display_name')
+DIR=$(echo "$input" | yq -r -p json '.workspace.current_dir')
 # The "// 0" provides a fallback if the field is null
-PCT=$(echo "$input" | jq -r '.context_window.used_percentage // 0' | cut -d. -f1)
+PCT=$(echo "$input" | yq -r -p json '.context_window.used_percentage // 0' | cut -d. -f1)
 
-IN=$(echo "$input" | jq -r '.context_window.total_input_tokens // 0')
-OUT=$(echo "$input" | jq -r '.context_window.total_output_tokens // 0')
+IN=$(echo "$input" | yq -r -p json '.context_window.total_input_tokens // 0')
+OUT=$(echo "$input" | yq -r -p json '.context_window.total_output_tokens // 0')
 
-RATE_USE=$(echo "$input" | jq -r '.rate_limits.five_hour.used_percentage // 0' | cut -d. -f1)
+RATE_USE=$(echo "$input" | yq -r -p json '.rate_limits.five_hour.used_percentage // 0' | cut -d. -f1)
 
 
 echo "[$MODEL] 📁 ${DIR##*/} | ${PCT}% context | ${IN} input | ${OUT} output | ${RATE_USE}% used"
