c9a6cccf1c0abf8f7b7670c11851b0c8e7266929
Joshuah Boardman <joshuahboardman@gmail.com>
2026-05-10 23:09:41 -0500

    Update for setting up new laptop

--- scripts/wayland/spotify-wl.sh
+++ scripts/wayland/spotify-wl.sh
@@ -5,8 +5,15 @@ export ELECTRON_ENABLE_WAYLAND=1
 export OZONE_PLATFORM=wayland
 export QT_QPA_PLATFORM=wayland
 export GDK_BACKEND=wayland,x11
+#export ELECTRON_FORCE_DEVICE_SCALE_FACTOR=1.5
 
 # Launch Spotify with Wayland window decorations enabled
-exec spotify \
+#exec spotify \
+  #--enable-features=WaylandWindowDecorations \
+  #--ozone-platform=wayland
+  #--force-device-scale-factor=1.5
+
+exec flatpak run com.spotify.Client \
   --enable-features=WaylandWindowDecorations \
-  --ozone-platform=wayland
+  --ozone-platform=wayland \
+#  --force-device-scale-factor=1.5
